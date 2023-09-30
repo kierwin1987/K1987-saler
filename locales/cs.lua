f2b3f6d11dad5ae45c7d4be9971cc9276486b9fe
kierwin1987 <37836859+kierwin1987@users.noreply.github.com>
2023-09-30 23:30:36 +0200

    Update cs.lua

--- locales/cs.lua
+++ locales/cs.lua
@@ -2,12 +2,10 @@ local Translations = {
     error = {
         ["invalid_items"] = "Nemáš správné itemy u sebe!",
         ["no_items"] = "Nemáš žédné item u sebe",
+		["not_enough_police"] = "Nedostatek PD",
     },
     success = {
 		["sold"] = "Prodal jsi %{value}x %{value2} za $%{value3}";
-	},
-	info = {
-		["open"] = "[E] otevřit";
 	}
 }
-Lang = Locale:new({phrases = Translations, warnOnMissing = true}
+Lang = Locale:new({phrases = Translations, warnOnMissing = true})
